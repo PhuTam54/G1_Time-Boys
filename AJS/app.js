68b811c5b0e4b62c13f92b9100663286df78061e
PhuTam54 <phutamytb@gmail.com>
2023-02-10 00:18:10 +0700

    Update

--- AJS/app.js
+++ AJS/app.js
@@ -21,7 +21,7 @@ validationApp.controller('mainController', function ($scope) {
 			name: "Tuan Kien",
 			email: "tuankien@gmail.com",
 			password: "123456",
-			phone: 01222222222
+			phone: 0122222222
 		}
 	]
 
@@ -55,35 +55,5 @@ validationApp.controller('mainController', function ($scope) {
 	$scope.clear = function () {
 		$scope.acc = {};
 		$scope.index = -1;
-	}
-
-	$scope.phoneNumbr = /^\+?\d{2}[- ]?\d{3}[- ]?\d{5}$/;
-
-	// function to submit the form after all validation has occurred			
-	$scope.submitForm = function (isValid) {
-
-		// check to make sure the form is completely valid
-		if (isValid) {
-			alert('Login Form is valid');
-		}
-	};
-
-	$scope.datas = [];
-	if (localStorage.getItem('user-list')) {
-		$scope.datas = angular.fromJson(localStorage.getItem('user-list'));
-	}
-	$scope.add_user = function () {
-		var user = [];
-		user.name = $scope.name;
-		user.email = $scope.email;
-		user.password = $scope.password;
-		user.phone = $scope.phone;
-		$scope.datas.push(user);
-		localStorage.setItem('user-list', angular.toJson($scope.datas));
-		console.log($scope.datas);
-	}
-	$scope.remove_user = function (i) {
-		$scope.datas.splice(i, 1);
-		localStorage.setItem('user-list', angular.toJson($scope.datas));
 	}
 });
